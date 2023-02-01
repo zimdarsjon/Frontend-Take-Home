50c86dae9a05960a90e609bd3fac1f124cd83749
Jonathon Zimdars <zimdarsj@kenyon.edu>
2023-02-01 13:02:46 -0600

    Added State Hooks

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,11 @@ const root = createRoot(document.getElementById("root"));
 const { useState, useEffect } = React;
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [occupation, setOccupation] = useState('');
+  const [state, setState = useState('')];
 
   return (
     <div>
