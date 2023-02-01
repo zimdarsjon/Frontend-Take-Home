592235cd1b2c9deb0601d617ea284bdf4e15f08d
Jonathon Zimdars <zimdarsj@kenyon.edu>
2023-02-01 15:30:50 -0600

    Bundled Production Package

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname + '/../public'));
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000');
+})
