5862dd8261be1a89ca4b2fd7e987b8bee6a0ea1e
Jonathon Zimdars <zimdarsj@kenyon.edu>
2023-02-01 14:22:20 -0600

    Validated Form Submission

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { getSelectionData, postFormData } from '../controls/controller.js';
-import Occupation from './inputs/Occupation.jsx';
-import State from './inputs/State.jsx';
+import { FormControl, Select, InputLabel, MenuItem, OutlinedInput, TextField, Button, Box, Alert, AlertTitle } from '@mui/material';
 
 const root = createRoot(document.getElementById("root"));
 
 const { useState, useEffect } = React;
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [occupation, setOccupation] = useState('');
-  const [state, setState] = useState('');
+  const [formFields, setFormFields] = useState({
+    name: '',
+    email: '',
+    password: '',
+    occupation: '',
+    state: ''
+  })
   const [stateOptions, setStateOptions] = useState([]);
   const [occupationOptions, setOccupationOptions] = useState([]);
+  const [alert, setAlert] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   useEffect(() => {
     getSelectionData()
@@ -25,17 +28,84 @@ const App = () => {
       })
   }, [])
 
+  const handleChange = (e) => {
+    setFormFields({ ...formFields, [e.target.name]: e.target.value})
+  }
+
+  const submit = () => {
+    let missingFields = [];
+    Object.keys(formFields).forEach(field => {
+      if (!formFields[field]) {
+        missingFields.push(field)
+      }
+    })
+    setAlert(missingFields.join(', '));
+    if (missingFields.length === 0) {
+      postFormData(formFields)
+        .then(() => setSubmitted(true))
+    }
+  }
+
+  if (submitted) {
+    return <h1>Complete</h1>
+  }
+
   return (
     <div>
-      <form>
-        <label htmlFor='occupations'>Choose an occupation:</label>
-        <select name='occupations' id='occupations' defaultValue='none'>
-          <option value='none' disabled hidden>Select an Option</option>
-          {occupationOptions.map(option => {
-            return <option key={option} value={option}>{option}</option>
+      <Box>
+      {alert &&
+      <Alert severity="error">
+        <AlertTitle>Missing Information</AlertTitle>
+        {alert}
+      </Alert>
+      }
+
+     <FormControl fullWidth>
+      <TextField id='name' label='Full Name' variant='standard' name='name' onChange={handleChange}/>
+     </FormControl>
+
+     <FormControl fullWidth>
+      <TextField id='email' label='Email' variant='standard' name='email' onChange={handleChange}/>
+     </FormControl>
+
+     <FormControl fullWidth>
+      <TextField type='password' id='password' label='Password' name='password' variant='standard' onChange={handleChange}/>
+     </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel id='occupation-label'>Occupation</InputLabel>
+        <Select
+          labelId='occupation-label'
+          id='occupations'
+          input={<OutlinedInput label='Occupation' />}
+          value={formFields.occupation}
+          onChange={handleChange}
+          name='occupation'
+        >
+          {occupationOptions.map(occupation => {
+            return <MenuItem key={occupation} value={occupation}>{occupation}</MenuItem>
           })}
-        </select>
-      </form>
+        </Select>
+      </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel id='state-label'>State</InputLabel>
+        <Select
+          labelId='state-label'
+          id='states'
+          input={<OutlinedInput label='State' />}
+          value={formFields.state}
+          onChange={handleChange}
+          name='state'
+          >
+          {stateOptions.map(state => {
+            return <MenuItem key={state.abbreviation} value={state.name}>{state.abbreviation}</MenuItem>
+          })}
+        </Select>
+      </FormControl>
+
+      <Button onClick={submit}>Submit</Button>
+      </Box>
     </div>
   )
 }
--- src/inputs/Occupation.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Occupation() {
-  return (
-    <h1>Occupation</h1>
-  )
-}
