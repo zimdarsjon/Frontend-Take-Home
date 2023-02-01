4f7341f9e6f886f2c04e34ffe16e1213c690daec
Jonathon Zimdars <zimdarsj@kenyon.edu>
2023-02-01 15:25:57 -0600

    Completion of front end

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { getSelectionData, postFormData } from '../controls/controller.js';
-import { FormControl, Select, InputLabel, MenuItem, OutlinedInput, TextField, Button, Box, Alert, AlertTitle } from '@mui/material';
+import { FormControl, Select, InputLabel, MenuItem, OutlinedInput, TextField, Button, Box, Alert, AlertTitle, Grow, Typography, Slide } from '@mui/material';
 
 const root = createRoot(document.getElementById("root"));
 
@@ -29,7 +29,7 @@ const App = () => {
   }, [])
 
   const handleChange = (e) => {
-    setFormFields({ ...formFields, [e.target.name]: e.target.value})
+    setFormFields({ ...formFields, [e.target.name]: e.target.value })
   }
 
   const submit = () => {
@@ -46,66 +46,84 @@ const App = () => {
     }
   }
 
-  if (submitted) {
-    return <h1>Complete</h1>
-  }
-
   return (
-    <div>
-      <Box>
-      {alert &&
-      <Alert severity="error">
-        <AlertTitle>Missing Information</AlertTitle>
-        {alert}
-      </Alert>
-      }
+    <div >
+      <Grow in={!submitted} direction='up' style={{ transitionDelay: '400ms' }}>
+        <Typography style={{fontSize: '4em', color: '#757de8'}} variant='h1' align="center">Account Creation</Typography>
+      </Grow>
+      <Grow in={!submitted} style={{ transitionDelay: '800ms' }}>
+        <Box sx={{
+          display: submitted ? 'block' : 'none',
+          p: 5,
+          border: '1px solid grey',
+          borderRadius: '5px',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '70vh',
+          width: '40%',
+          justifyContent: 'space-between',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }}>
+          <Grow in={alert.length > 0}>
+            <Alert style={{display: alert ? 'block' : 'none'}} severity="error">
+              <AlertTitle>Missing Information</AlertTitle>
+              {alert}
+            </Alert>
+          </Grow>
 
-     <FormControl fullWidth>
-      <TextField id='name' label='Full Name' variant='standard' name='name' onChange={handleChange}/>
-     </FormControl>
+          <FormControl fullWidth>
+            <TextField id='name' label='Full Name' variant='standard' name='name' onChange={handleChange} />
+          </FormControl>
 
-     <FormControl fullWidth>
-      <TextField id='email' label='Email' variant='standard' name='email' onChange={handleChange}/>
-     </FormControl>
+          <FormControl fullWidth>
+            <TextField id='email' label='Email' variant='standard' name='email' onChange={handleChange} />
+          </FormControl>
 
-     <FormControl fullWidth>
-      <TextField type='password' id='password' label='Password' name='password' variant='standard' onChange={handleChange}/>
-     </FormControl>
+          <FormControl fullWidth>
+            <TextField type='password' id='password' label='Password' name='password' variant='standard' onChange={handleChange} />
+          </FormControl>
 
-      <FormControl fullWidth>
-        <InputLabel id='occupation-label'>Occupation</InputLabel>
-        <Select
-          labelId='occupation-label'
-          id='occupations'
-          input={<OutlinedInput label='Occupation' />}
-          value={formFields.occupation}
-          onChange={handleChange}
-          name='occupation'
-        >
-          {occupationOptions.map(occupation => {
-            return <MenuItem key={occupation} value={occupation}>{occupation}</MenuItem>
-          })}
-        </Select>
-      </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id='occupation-label'>Occupation</InputLabel>
+            <Select
+              labelId='occupation-label'
+              id='occupations'
+              input={<OutlinedInput label='Occupation' />}
+              value={formFields.occupation}
+              onChange={handleChange}
+              name='occupation'
+            >
+              {occupationOptions.map(occupation => {
+                return <MenuItem key={occupation} value={occupation}>{occupation}</MenuItem>
+              })}
+            </Select>
+          </FormControl>
 
-      <FormControl fullWidth>
-        <InputLabel id='state-label'>State</InputLabel>
-        <Select
-          labelId='state-label'
-          id='states'
-          input={<OutlinedInput label='State' />}
-          value={formFields.state}
-          onChange={handleChange}
-          name='state'
-          >
-          {stateOptions.map(state => {
-            return <MenuItem key={state.abbreviation} value={state.name}>{state.abbreviation}</MenuItem>
-          })}
-        </Select>
-      </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id='state-label'>State</InputLabel>
+            <Select
+              labelId='state-label'
+              id='states'
+              input={<OutlinedInput label='State' />}
+              value={formFields.state}
+              onChange={handleChange}
+              name='state'
+            >
+              {stateOptions.map(state => {
+                return <MenuItem key={state.abbreviation} value={state.name}>{state.abbreviation}</MenuItem>
+              })}
+            </Select>
+          </FormControl>
 
-      <Button onClick={submit}>Submit</Button>
-      </Box>
+          <Button onClick={submit}>Submit</Button>
+        </Box>
+      </Grow>
+      <Grow in={submitted} style={{ transitionDelay: '400ms' }} appear>
+          <Box style={{position: 'absolute', left: '30%', top: '40vh'}}>
+            <Typography style={{fontSize: '4em', color: '#757de8'}} variant='h1' align="center">User Account Created</Typography>
+          </Box>
+        </Grow>
     </div>
   )
 }
